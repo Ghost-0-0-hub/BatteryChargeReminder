17e1f434291c8f08d6d4bc4415b5a0b34ee5488d
Ghost-0-0-hub <145931860+Ghost-0-0-hub@users.noreply.github.com>
2024-11-04 00:08:01 +0500

    This is a Full Battery Notification program
    This program notifies you via pop-up of a text file, that your laptop is 100% charged.

---
+++ batterychecker.py
@@ -0,0 +1,16 @@
+import psutil
+import os
+
+battery = psutil.sensors_battery()
+
+if battery is not None:
+    if battery.percent == 100 and battery.power_plugged:
+        print('your battery is 100%')
+        desktop_path = os.path.join(os.path.expanduser('~'),"Desktop","BatteryReminder.txt")
+        with open(desktop_path,'w') as file:
+            file.write('your battery is 100%, remove Charging')
+
+        try:
+            os.startfile(desktop_path)
+        except:
+            print(f'Reminder saved at {desktop_path}')
